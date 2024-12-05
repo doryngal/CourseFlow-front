cd870bdfb12c7a8386b3cb77d902a0cc39f800bf
QTemir <tigergreen08@gmail.com>
2024-12-05 11:48:29 +0600

    Front of Login and Register

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,23 @@
-// pages/ProfilePage.jsx
+import React from 'react';
 import UserProfile from '../components/Profile/UserProfile';
 import { Container } from '@mui/material';
+import styles from './ProfilePage.module.css'; // Импорт стилей
 
 function ProfilePage() {
     return (
-        <Container>
-            <UserProfile />
-        </Container>
+        <div className={styles.profilePageContainer}>
+            <Container className={styles.profileContent}>
+                <div className={styles.userProfile}>
+                    <h1 className={styles.profileTitle}>Ваш профиль</h1>
+                    <UserProfile />
+                </div>
+            </Container>
+            <div className={styles.footer}>
+                <p>© 2024 ENU University. Все права защищены.</p>
+                <a href="#privacy-policy">Политика конфиденциальности</a>
+                <a href="#terms-of-service">Условия использования</a>
+            </div>
+        </div>
     );
 }
 
